4fe1a734726130d0ae18658c43a12653ffad3796
Dylan2912 <dlopezm@insgabrielamistral.cat>
2026-03-06 22:35:54 +0100

    Update Main.java

--- ProjecteGit/src/main/Main.java
+++ ProjecteGit/src/main/Main.java
@@ -5,6 +5,8 @@ public class Main {
         System.out.println("1a modificació Git");
 		System.out.println("2a modificació Git");
 		System.out.println("3a modificació Git");
+		System.out.println("4a modificació Git branca2");
     }
 }
+
 
